5c52f4a834247931db8097a79f946532e5c6d349
lanfangping <fangpinglan0116@gmail.com>
2021-08-09 22:07:48 -0400

    Update test.sql

--- apps/test.sql
+++ apps/test.sql
@@ -11,9 +11,3 @@ CREATE UNLOGGED TABLE test_policy (
        condition TEXT[],
        PRIMARY key (fid)
 );
-
--- CREATE OR REPLACE VIEW test_violation AS (
---        SELECT rm.fid, rate AS req, vol AS asgn
---        FROM rm, test_policy
---        WHERE rm.fid = test_policy.fid AND rate > vol
--- );
