a5af5a876ead1e89052ac869c2c056bf18f46fe4
lanfangping <fangpinglan0116@gmail.com>
2021-08-14 12:50:50 -0400

    Update relational_algebra.sql

--- apps/relational_algebra.sql
+++ apps/relational_algebra.sql
@@ -1,8 +1,20 @@
+------------------------------------------------------------
+-- Relational Algebra Application
+------------------------------------------------------------
+
+/* This doesn't do anything interesting, just create a table */
+DROP TABLE IF EXISTS relational_algebra CASCADE;
+CREATE UNLOGGED TABLE relational_algebra (
+       id  integer PRIMARY KEY
+);
 
 /* Violation: */
-CREATE OR REPLACE VIEW relational_algebra_violation;
+CREATE OR REPLACE VIEW relational_algebra_violation AS (
+       SELECT id
+       FROM relational_algebra
+);
 
 /* Repair */
-CREATE OR REPLACE RULE bgp_repair AS
-       ON DELETE TO bgp_violation
+CREATE OR REPLACE RULE relational_algebra_repair AS
+       ON DELETE TO relational_algebra_violationAS
        DO NOTHING;
