c5192ed05fe4b050b8cc6dd6aebf62b40cc09b85
lanfangping <fangpinglan0116@gmail.com>
2021-08-14 14:35:21 -0400

    Update relational_algebra.sql
    add demo tables

--- apps/relational_algebra.sql
+++ apps/relational_algebra.sql
@@ -8,6 +8,42 @@ CREATE UNLOGGED TABLE relational_algebra (
        id  integer PRIMARY KEY
 );
 
+/* DEMOs - ctables
+ * Policy1: Static Routes, Filter
+ * dest: IP prefix
+ * path:
+ * condition:
+ */
+DROP TABLE IF EXISTS policy1 CASCADE;
+CREATE UNLOGGED TABLE policy1(
+       dest TEXT,
+       path TEXT,
+       condition TEXT[]
+);
+INSERT INTO Policy1 (dest, path, condition) VALUES 
+('1.2.3.4','x','{"x == [ABC]"}'),
+('y','z','{"y != 1.2.3.5", "y != 1.2.3.4"}');
+
+/* DEMOs - ctables
+ * Policy2: Traffic Balancer
+ * dest: IP prefix
+ * path:
+ * condition:
+ */
+DROP TABLE IF EXISTS Policy2 CASCADE;
+create table Policy2 ( 
+       dest TEXT, 
+       path TEXT, 
+       flag TEXT, 
+       condition TEXT[]
+);
+INSERT INTO Policy2 (dest, path, flag, condition) VALUES 
+('1.2.3.4','[ABC]', 'u', '{"u == 1"}'),
+('5.6.7.8','[ABC]', 'u', '{"u != 1"}'),
+('1.2.3.4','[AC]', 'v', '{"v == 1"}'),
+('5.6.7.8','[AC]', 'v', '{"v != 1"}');
+
+
 /* Violation: */
 CREATE OR REPLACE VIEW relational_algebra_violation AS (
        SELECT id
