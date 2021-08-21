68f3e3552e5ced7e7eb65520aa0c06b5fa328116
lanfangping <fangpinglan0116@gmail.com>
2021-08-21 10:03:29 -0400

    Update sarasate.py

--- apps/sarasate.py
+++ apps/sarasate.py
@@ -5,22 +5,22 @@ import re
 from z3 import *
 
 class RelaAlgConsole(AppConsole):
-    def do_echo(self, line):
-        print("test", line)
 
     def default(self, line):
         "Execute a PostgreSQL statement"
         try:
         
-            data, condition, z3 = self._get_sql(line)
+            # data, condition, z3 = self._get_sql(line)
 
-            for d in data:
-                self.db.cursor.execute(d)
-            for c in condition:
-                if c != '':
-                    self.db.cursor.execute(c)
-            for z in z3:
-                self.db.cursor.execute(z)
+            # for d in data:
+            #     self.db.cursor.execute(d)
+            # for c in condition:
+            #     if c != '':
+            #         self.db.cursor.execute(c)
+            # for z in z3:
+            #     self.db.cursor.execute(z)
+            select_clause, from_clause, defined_where_clause, where_lists = self.pre_processing(line)
+            self.generator(select_clause, from_clause, defined_where_clause, where_lists)
 
         except psycopg2.ProgrammingError as e:
             print(e)
@@ -37,6 +37,184 @@ class RelaAlgConsole(AppConsole):
             pass
         except TypeError as e:
             print(e)
+    
+    def pre_processing(self, query):
+        # remove ;
+        if ';' in query:
+            query = query[:-1]
+
+        query_lower = query.lower()
+
+        # detect the location of where
+        where_index = query_lower.find('where')
+        where_clause = query[where_index+5:]
+
+        select_clause = query_lower[ :where_index]
+
+        # get the tables
+        pattern = re.compile(r'from(.*?)where', re.S)
+        from_clause = re.findall(pattern, query_lower)[0].strip()
+
+        '''
+        Processing comparison operators
+        '''
+        defined_where_clause = ""
+        where_lists = re.split("and", where_clause)
+        for w in where_lists:
+            # if 'len' in w, that means this columm's type is integer
+            if 'len' in w:
+                continue
+
+            if '!=' in w:
+                args = w.split('!=')
+                left = args[0].strip()
+                right = args[1].strip()
+                defined_where_clause = defined_where_clause + "not_equal({}, {}) and".format(left, right) 
+            elif '<>' in w:
+                args = w.split('<>')
+                left = args[0].strip()
+                right = args[1].strip()
+                defined_where_clause = defined_where_clause + "not_equal({}, {}) and ".format(left, right) 
+            elif '<=' in w:
+                args = w.split('<=')
+                left = args[0].strip()
+                right = args[1].strip()
+                defined_where_clause = defined_where_clause + "leq({}, {}) and ".format(left, right)
+            elif '>=' in w:
+                args = w.split('>=')
+                left = args[0].strip()
+                right = args[1].strip()
+                defined_where_clause = defined_where_clause + "geq({}, {}) and ".format(left, right)
+            elif '<' in w:
+                args = w.split('<')
+                left = args[0].strip()
+                right = args[1].strip()
+                defined_where_clause = defined_where_clause + "less({}, {}) and ".format(left, right)
+            elif '>' in w:
+                args = w.split('>')
+                left = args[0].strip()
+                right = args[1].strip()
+                defined_where_clause = defined_where_clause + "greater({}, {}) and ".format(left, right)
+            elif '=' in w:
+                args = w.split('=')
+                left = args[0].strip()
+                right = args[1].strip()
+                defined_where_clause = defined_where_clause + "equal({}, {}) and ".format(left, right)
+
+        defined_where_clause = defined_where_clause[:-4]  # remove final 'and'      
+
+        return select_clause, from_clause, defined_where_clause, where_lists
+
+    def generator(self, select_clause, from_clause, where_clause, where_lists):
+        '''
+        The number of tables is greater than 1, it is join operation
+        else, it is selection
+        '''
+        table_list = from_clause.split(',')
+        if len(table_list) > 1:
+
+            print('join')
+            t1_name = table_list[0].strip()
+            t2_name = table_list[1].strip()
+            
+            if '*' in select_clause:
+                '''
+                get the attributes of each table
+                '''
+                self.db.cursor.execute("select * from {}".format(t1_name))
+                t1_attrs = [row[0] for row in self.db.cursor.description]
+                self.db.cursor.execute("select * from {}".format(t2_name))
+                t2_attrs = [row[0] for row in self.db.cursor.description]
+                
+                '''
+                get common attributes and difference attributes
+                '''
+                common_attr = set(t1_attrs).intersection(set(t2_attrs)) - set(['condition'])
+                union_attr = set(t1_attrs).union(set(t2_attrs)) - set(['condition'])
+                diff_attr = union_attr - common_attr
+
+                print(common_attr)
+                print(diff_attr)
+
+                attr_diff = ""
+                attr_equal = ""
+                
+                for c in common_attr:
+                    attr_equal += "{}.{}, {}.{} AS {}_{},".format(t1_name, c, t2_name, c, t2_name, c)
+                    
+                for d in diff_attr:
+                    attr_diff += " {},".format(d)
+
+                attr_diff += "array_cat({}.condition, {}.condition) as condition".format(t1_name, t2_name)
+
+                print("Step1: Create Data Content")
+                sql = "create table output as select {} {} FROM {} where ".format(attr_equal, attr_diff, from_clause) + where_clause
+                print(sql)
+                self.db.cursor.execute(sql)
+
+                print("Step2: Update Condition")
+
+                for w in where_lists:
+                    args = w.strip().split(' ')
+                    left = args[0].strip()
+                    opr = args[1].strip()
+                    right = args[2].strip()
+                    # repalce = with == in order accommodate z3
+                    if '!=' not in opr and '<=' not in opr and '>=' not in opr and '=' in opr:
+                        opr = opr.replace('=', '==')
+                    
+                    sql = "update output set conditon = array_append(condition, {} || '{}' || {});".format(left, opr, right)
+                    self.db.cursor.execute(sql)
+
+                attr_drop = ""
+                for c in common_attr:
+                    sql = "update output set {} = {}_{} where not is_var({})".format(c, t2_name, c, c)
+                    attr_drop = attr_drop + "drop cloumn {}_{}, ".format(t2_name, c)
+                    print(sql)
+                    self.db.cursor.execute(sql)
+
+                # remove the spare ,
+                attr_drop = attr_drop[:-2]
+                sql = "alter table output {};".format(attr_drop)
+                print(sql)
+                self.db.cursor.execute(sql)
+
+            else:
+                print("still working")   
+        else:
+            print('selection')
+            print('Step1: Create Data Content')
+            sql = 'create table output as '
+
+            sql = sql + select_clause + ' where '+ where_clause + ';'
+            print(sql)
+            self.db.cursor.execute(sql)
+
+            print('Step2: Update Condition')
+            for w in where_lists:
+                args = w.strip().split(' ')
+                left = args[0].strip()
+                opr = args[1].strip()
+                right = args[2].strip()
+                # repalce = with == in order accommodate z3
+                if '!=' not in opr and '<=' not in opr and '>=' not in opr and '=' in opr:
+                    opr = opr.replace('=', '==')
+                
+                sql = "update output set conditon = array_append(condition, {} || '{}' || {});".format(left, opr, right)
+                print(sql)
+                self.db.cursor.execute(sql)
+
+
+        print('Step3: Normalization')
+        sql = 'delete from output where is_contradiction(condition);'
+        print(sql)
+        self.db.cursor.execute(sql)
+        sql = "UPDATE output SET condition = '{}' WHERE is_tauto(condition);"
+        print(sql)
+        self.db.cursor.execute(sql)
+        sql = "UPDATE output SET condition = remove_redundant(condition) WHERE has_redundant(condition);"
+        print(sql)
+        self.db.cursor.execute(sql)
 
     def do_data(self, line):
         "Create data content."
