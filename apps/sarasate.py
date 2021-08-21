8da26d553a0b86de16cac5f6ebed9d608fbdf4b4
lanfangping <fangpinglan0116@gmail.com>
2021-08-21 10:07:13 -0400

    Update sarasate.py

--- apps/sarasate.py
+++ apps/sarasate.py
@@ -163,7 +163,7 @@ class RelaAlgConsole(AppConsole):
                     if '!=' not in opr and '<=' not in opr and '>=' not in opr and '=' in opr:
                         opr = opr.replace('=', '==')
                     
-                    sql = "update output set conditon = array_append(condition, {} || '{}' || {});".format(left, opr, right)
+                    sql = "update output set condition = array_append(condition, {} || '{}' || {});".format(left, opr, right)
                     self.db.cursor.execute(sql)
 
                 attr_drop = ""
