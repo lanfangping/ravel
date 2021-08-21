ff134db85c1c5a86657a19aa853a6ec1ee13b93d
lanfangping <fangpinglan0116@gmail.com>
2021-08-21 14:34:42 -0400

    Update bgp.py

--- apps/bgp.py
+++ apps/bgp.py
@@ -186,7 +186,8 @@ class BGPConsole(AppConsole):
                     self.db.cursor.execute(sql)
 
                 # remove the spare ,
-                attr_drop = attr_drop[:-2]
+                # attr_drop = attr_drop[:-2]
+                attr_drop = attr_drop + "drop column len_path"
                 sql = "alter table output {};".format(attr_drop)
                 self.db.cursor.execute(sql)
 
